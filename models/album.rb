124b12789b9a3ab3a314ea51f33055e39c027644
Craig McLeod <mcleod.craig@outlook.com>
2020-05-27 17:05:36 +0100

    Final commit, extensions finished

--- models/album.rb
+++ models/album.rb
@@ -50,6 +50,17 @@ class Album
         SqlRunner.run(sql, values)
     end
 
+    def self.find(id)
+        sql = "SELECT *
+        FROM albums
+        WHERE id = $1"
+        values = [id]
+        result = SqlRunner.run(sql, values)
+        album_hash = result.first
+        album = Album.new(album_hash)
+        return album
+    end
+
     def self.all
         sql = "SELECT * FROM albums"
         album_hashes = SqlRunner.run(sql) 
