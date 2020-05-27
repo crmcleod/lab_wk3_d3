db4e9527f48e7be533e093e67f9487fb0797f6cb
Craig McLeod <mcleod.craig@outlook.com>
2020-05-27 16:51:19 +0100

    Defined delete instance for albums

--- models/album.rb
+++ models/album.rb
@@ -43,6 +43,13 @@ class Album
         SqlRunner.run(sql, values)
     end
 
+    def delete()
+        sql = "DELETE FROM albums
+        WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
     def self.all
         sql = "SELECT * FROM albums"
         album_hashes = SqlRunner.run(sql) 
