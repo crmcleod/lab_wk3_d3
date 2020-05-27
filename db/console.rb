442c0ec470c19c53e5abbc35fcb39a8196ddf35f
Craig McLeod <mcleod.craig@outlook.com>
2020-05-27 15:01:54 +0100

    Populated console

---
+++ db/console.rb
@@ -0,0 +1,23 @@
+require ('pry')
+require_relative("../models/album")
+require_relative("../models/artist")
+
+Album.delete_all()
+Artist.delete_all()
+
+artist1 = Artist.new({
+    "name" => "Abba"
+})
+
+artist.save()
+
+album1 = Album.new({
+    "title" => "Ring Ring",
+    "genre" => "Pop",
+    "artist_id" => artist1.id
+})
+
+album1.save()
+
+binding.pry
+nil
